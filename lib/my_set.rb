be9cbdbd0e5cc4609b133d9f0cbcbacbc2e9b29f
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-11 10:57:11 -0400

    commit tree 42d369a892936e3b86aa9d72bc385f961d2dfd9e

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,29 @@
 # your code here
+class MySet
+
+  def initialize(enumerable = [])
+    @hash = {}
+    enumerable.each do |value|
+      @hash[value] = true
+    end
+  end
+
+  def include?(value)
+    @hash.has_key?(value)
+  end
+
+  def add(value)
+    @hash[value] = true
+    self
+  end
+
+  def delete(value)
+    @hash.delete(value)
+    self
+  end
+
+  def size
+    @hash.size
+  end
+
+end
